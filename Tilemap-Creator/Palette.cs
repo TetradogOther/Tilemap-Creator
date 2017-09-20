4a8d445e959bd780183201d2e05613e05aa5b31a
Gabriel <tetradog@gmail.com>
2017-09-20 15:59:40 +0200

    acomodando el codigo

--- Tilemap-Creator/Palette.cs
+++ Tilemap-Creator/Palette.cs
@@ -8,198 +8,215 @@ using System.Threading.Tasks;
 
 namespace TMC
 {
-    public enum PaletteFormat
-    {
-        /// <summary>
-        /// Photoshop Palette
-        /// </summary>
-        PAL,
-        /// <summary>
-        /// Adobe Color Table
-        /// </summary>
-        ACT,
-        /// <summary>
-        /// Nitro Color Table
-        /// </summary>
-        NCLR,
-        /// <summary>
-        /// APE Palette
-        /// </summary>
-        GPL, // GIMP palette uses the same extension!!!
-    }
+	public enum PaletteFormat
+	{
+		/// <summary>
+		/// Photoshop Palette
+		/// </summary>
+		PAL,
+		/// <summary>
+		/// Adobe Color Table
+		/// </summary>
+		ACT,
+		/// <summary>
+		/// Nitro Color Table
+		/// </summary>
+		NCLR,
+		/// <summary>
+		/// APE Palette
+		/// </summary>
+		GPL, // GIMP palette uses the same extension!!!
+	}
 
-    public static class ColorExtensions
-    {
-        public static ushort ToColor15(this Color color)
-        {
-            return (ushort)((color.R >> 3) | ((color.G >> 3) << 5) | ((color.B >> 3) << 10));
-        }
-    }
+	public static class ColorExtensions
+	{
+		public static ushort ToColor15(this Color color)
+		{
+			return (ushort)((color.R >> 3) | ((color.G >> 3) << 5) | ((color.B >> 3) << 10));
+		}
+	}
 
-    public static class Palette
-    {
-        public static Color[] Load(string filename, PaletteFormat format)
-        {
-            switch (format)
-            {
-                case PaletteFormat.ACT:
-                    return LoadAct(filename);
-                case PaletteFormat.PAL:
-                    return LoadPal(filename);
+	public static class Palette
+	{
+		const int LENGHT=256;
+		
+		public static Color[] Load(string filename, PaletteFormat format)
+		{
+			switch (format)
+			{
+				case PaletteFormat.ACT:
+					return LoadAct(filename);
+				case PaletteFormat.PAL:
+					return LoadPal(filename);
 
-                default:
-                    throw new NotImplementedException();
-            }
-        }
+				default:
+					throw new NotImplementedException();
+			}
+		}
 
-        static Color[] LoadAct(string filename)
-        {
-            var colors = new Color[256];
+		static Color[] LoadAct(string filename)
+		{
+			Color[] colors = new Color[LENGHT];
+			
+			int r;
+			int g;
+			int b;
+			bool encontrado;
+			
+			using (FileStream fs = File.OpenRead(filename))
+			{
+				encontrado=false;
+				for (int i = 0; i < LENGHT&&!encontrado; i++)
+				{
+					r = fs.ReadByte();
+					g = fs.ReadByte();
+					b = fs.ReadByte();
 
-            using (var fs = File.OpenRead(filename))
-            {
-                for (int i = 0; i < 256; i++)
-                {
-                    var r = fs.ReadByte();
-                    var g = fs.ReadByte();
-                    var b = fs.ReadByte();
+					if (r == -1 || g == -1 || b == -1)
+						encontrado=true;
+					else
+						colors[i] = Color.FromArgb(r, g, b);
+				}
+			}
 
-                    if (r == -1 || g == -1 || b == -1)
-                        break;
+			return colors;
+		}
 
-                    colors[i] = Color.FromArgb(r, g, b);
-                }
-            }
+		static Color[] LoadPal(string filename)
+		{
+			string[] c;
+			int r;
+			int g;
+			int b;
+			int size;
+			Color[] colors;
+			using (var sr = File.OpenText(filename))
+			{
+				if (sr.ReadLine() != "JASC-PAL")
+					throw new Exception();
+				if (sr.ReadLine() != "0100")
+					throw new Exception();
 
-            return colors;
-        }
+				size = int.Parse(sr.ReadLine());
+				colors = new Color[size];
 
-        static Color[] LoadPal(string filename)
-        {
-            using (var sr = File.OpenText(filename))
-            {
-                if (sr.ReadLine() != "JASC-PAL")
-                    throw new Exception();
-                if (sr.ReadLine() != "0100")
-                    throw new Exception();
+				for (int i = 0; i < size; i++)
+				{
+					c = sr.ReadLine().Split(' ');
 
-                var size = int.Parse(sr.ReadLine());
-                var colors = new Color[size];
+					// no safe checking, we want an exception on error
+					r = int.Parse(c[0]);
+					g = int.Parse(c[1]);
+					b = int.Parse(c[2]);
+					colors[i] = Color.FromArgb(r, g, b);
+				}
 
-                for (int i = 0; i < size; i++)
-                {
-                    var c = sr.ReadLine().Split(' ');
+				return colors;
+			}
+		}
 
-                    // no safe checking, we want an exception on error
-                    var r = int.Parse(c[0]);
-                    var g = int.Parse(c[1]);
-                    var b = int.Parse(c[2]);
-                    colors[i] = Color.FromArgb(r, g, b);
-                }
+		public static void Save(Color[] colors, string filename, PaletteFormat format)
+		{
+			switch (format)
+			{
+				case PaletteFormat.PAL:
+					SavePal(colors, filename);
+					break;
+				case PaletteFormat.ACT:
+					SaveAct(colors, filename);
+					break;
+				case PaletteFormat.GPL:
+					SaveGpl(colors, filename);
+					break;
 
-                return colors;
-            }
-        }
+				default:
+					throw new NotSupportedException();
+			}
+		}
 
-        public static void Save(Color[] colors, string filename, PaletteFormat format)
-        {
-            switch (format)
-            {
-                case PaletteFormat.PAL:
-                    SavePal(colors, filename);
-                    break;
-                case PaletteFormat.ACT:
-                    SaveAct(colors, filename);
-                    break;
-                case PaletteFormat.GPL:
-                    SaveGpl(colors, filename);
-                    break;
+		static void SavePal(Color[] colors, string filename)
+		{
+			// NOTE: convention states JASC-PAL files should only have
+			// 256 or 16 colors, but we'll allow any number
 
-                default:
-                    throw new NotSupportedException();
-            }
-        }
+			//if (colors.Length > LENGHT)
+			//    throw new Exception("The given palette has too many colors for a palette file!");
 
-        static void SavePal(Color[] colors, string filename)
-        {
-            // NOTE: convention states JASC-PAL files should only have
-            // 256 or 16 colors, but we'll allow any number
+			using (var sw = File.CreateText(filename))
+			{
+				// header
+				sw.WriteLine("JASC-PAL");
+				sw.WriteLine("0100");
+				sw.WriteLine(colors.Length);
 
-            //if (colors.Length > 256)
-            //    throw new Exception("The given palette has too many colors for a palette file!");
+				// colors
+				for (int i = 0; i < colors.Length; i++)
+				{
+					sw.WriteLine("{0} {1} {2}", colors[i].R, colors[i].G, colors[i].B);
+				}
+			}
+		}
 
-            using (var sw = File.CreateText(filename))
-            {
-                // header
-                sw.WriteLine("JASC-PAL");
-                sw.WriteLine("0100");
-                sw.WriteLine(colors.Length);
+		static void SaveAct(Color[] colors, string filename)
+		{
+			// an Adobe Color Table must always be 256 colors
+			// http://www.adobe.com/devnet-apps/photoshop/fileformatashtml/#50577411_pgfId-1070626
 
-                // colors
-                for (int i = 0; i < colors.Length; i++)
-                {
-                    sw.WriteLine("{0} {1} {2}", colors[i].R, colors[i].G, colors[i].B);
-                }
-            }
-        }
+			if (colors.Length > LENGHT)
+				throw new Exception("The given palette has too many colors for an Adobe Color Table!");
 
-        static void SaveAct(Color[] colors, string filename)
-        {
-            // an Adobe Color Table must always be 256 colors
-            // http://www.adobe.com/devnet-apps/photoshop/fileformatashtml/#50577411_pgfId-1070626
+			using (var bw = new BinaryWriter(File.Create(filename)))
+			{
+				for (int i = 0; i < LENGHT; i++)
+				{
+					if (i < colors.Length)
+					{
+						bw.Write(colors[i].R);
+						bw.Write(colors[i].G);
+						bw.Write(colors[i].B);
+					}
+					else
+					{
+						bw.Write(byte.MinValue);
+						bw.Write(byte.MinValue);
+						bw.Write(byte.MinValue);
+					}
+				}
+			}
+		}
 
-            if (colors.Length > 256)
-                throw new Exception("The given palette has too many colors for an Adobe Color Table!");
+		static void SaveGpl(Color[] colors, string filename)
+		{
+			// this is a truely HORRIBLE format for storing a palette
+			// no clue what HackMew was thinking honestly
+			
+			short color;
+			
+			if (colors.Length > LENGHT)
+				throw new Exception("The given palette has too many colors for an APE palette!");
 
-            using (var bw = new BinaryWriter(File.Create(filename)))
-            {
-                for (int i = 0; i < 256; i++)
-                {
-                    if (i < colors.Length)
-                    {
-                        bw.Write(colors[i].R);
-                        bw.Write(colors[i].G);
-                        bw.Write(colors[i].B);
-                    }
-                    else
-                    {
-                        bw.Write(byte.MinValue);
-                        bw.Write(byte.MinValue);
-                        bw.Write(byte.MinValue);
-                    }
-                }
-            }
-        }
+			using (var sw = File.CreateText(filename))
+			{
+				sw.WriteLine("[APE Palette]");
 
-        static void SaveGpl(Color[] colors, string filename)
-        {
-            // this is a truely HORRIBLE format for storing a palette
-            // no clue what HackMew was thinking honestly
-            if (colors.Length > 256)
-                throw new Exception("The given palette has too many colors for an APE palette!");
+				for (int i = 0; i < LENGHT; i++)
+				{
+					// convert color to GBA format
+					// BUT, we want it signed AND bytes flipped
+					color = 0;
+					if (i < colors.Length)
+						color = colors[i].ToApeColor();
 
-            using (var sw = File.CreateText(filename))
-            {
-                sw.WriteLine("[APE Palette]");
+					// ugly thing isn't it
+					sw.WriteLine("{0}{1} ", color < 0 ? "" : " ", color);
+				}
+			}
+		}
 
-                for (int i = 0; i < 256; i++)
-                {
-                    // convert color to GBA format
-                    // BUT, we want it signed AND bytes flipped
-                    short color = 0;
-                    if (i < colors.Length)
-                        color = colors[i].ToApeColor();
-
-                    // ugly thing isn't it
-                    sw.WriteLine("{0}{1} ", color < 0 ? "" : " ", color);
-                }
-            }
-        }
-
-        static short ToApeColor(this Color c)
-        {
-            var g = c.ToColor15();
-            return (short)(((g & 0xFF) << 8) | (g >> 8));
-        }
-    }
+		static short ToApeColor(this Color c)
+		{
+			ushort g = c.ToColor15();
+			return (short)(((g & 0xFF) << 8) | (g >> 8));
+		}
+	}
 }
